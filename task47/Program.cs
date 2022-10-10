8dc4ce98ba79eab2cd35cd53ddd1a923933ac178
Bormental <tyranosaurus@yandex.ru>
2022-10-10 19:12:58 +0300

    task47 solved

---
+++ task47/Program.cs
@@ -0,0 +1,51 @@
+﻿/* 
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] Generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    double[,] twoDArray = new double[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = Math.Round(new Random().NextDouble()*(randomEnd - randomStart) + randomStart, 2);
+        }
+    }
+    return twoDArray;
+}
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void Print2DArray(double[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        PrintColorData(i + "\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        PrintColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+double[,] newGeneratedArray = Generate2DArray(5, 7, -9, 9);
+Print2DArray(newGeneratedArray);
+
+
